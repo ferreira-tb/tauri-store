786c127a8b90060b5113afe4bfdba574bdd70a8f
Andrew Ferreira <andrew.shien2@gmail.com>
2024-09-27 19:54:50 -0300

    feat: add `filterKeys` option

--- packages/tauri-plugin-pinia/src/state.ts
+++ packages/tauri-plugin-pinia/src/state.ts
@@ -1,12 +1,15 @@
-import type { State, TauriPluginPiniaStoreOptions } from './types';
+import type { State, StoreKeyFilter, TauriPluginPiniaStoreOptions } from './types';
 
 export function applyOptions(state: State, options?: TauriPluginPiniaStoreOptions): State {
-  if (options?.ignoreKeys) {
+  if (options?.filterKeys) {
     const result: State = {};
+    const { filterKeysStrategy = 'omit' } = options;
+
     for (const [key, value] of Object.entries(state)) {
       if (
-        (typeof options.ignoreKeys === 'string' && key === options.ignoreKeys) ||
-        (Array.isArray(options.ignoreKeys) && options.ignoreKeys.includes(key))
+        (filterKeysStrategy === 'omit' && isStoreKeyMatch(options.filterKeys, key)) ||
+        (filterKeysStrategy === 'pick' && !isStoreKeyMatch(options.filterKeys, key)) ||
+        (typeof filterKeysStrategy === 'function' && !filterKeysStrategy(key))
       ) {
         continue;
       }
@@ -18,4 +21,12 @@ export function applyOptions(state: State, options?: TauriPluginPiniaStoreOption
   }
 
   return state;
+}
+
+function isStoreKeyMatch(filter: StoreKeyFilter, key: string): boolean {
+  return (
+    (typeof filter === 'string' && key === filter) ||
+    (Array.isArray(filter) && filter.includes(key)) ||
+    (filter instanceof RegExp && filter.test(key))
+  );
 }
--- packages/tauri-plugin-pinia/src/types.ts
+++ packages/tauri-plugin-pinia/src/types.ts
@@ -3,7 +3,11 @@ import type { MaybePromise } from '@tb-dev/utils';
 export interface TauriPluginPiniaOptions {
   /** @default 0 */
   readonly debounce?: number;
-  /** @default true */
+
+  /**
+   * Whether the store should be deeply watched for changes.
+   * @default true
+   */
   readonly deep?: boolean;
 
   /**
@@ -13,8 +17,33 @@ export interface TauriPluginPiniaOptions {
   readonly onError?: (error: unknown) => MaybePromise<void>;
 }
 
+export type StoreKeyFilter = string | string[] | RegExp;
+export type StoreKeyFilterStrategy = 'pick' | 'omit' | ((key: string) => boolean);
+
 export interface TauriPluginPiniaStoreOptions extends TauriPluginPiniaOptions {
-  /** Keys the plugin should ignore. Those won't be saved nor synced. */
+  /**
+   * Keys the plugin should save or ignore.
+   *
+   * The behavior depends on the value of {@link TauriPluginPiniaStoreOptions.filterKeysStrategy}.
+   */
+  readonly filterKeys?: StoreKeyFilter;
+
+  /**
+   * Strategy to use when filtering keys.
+   * - `pick`: Only the specified keys will be synced and saved.
+   * - `omit`: All keys will be synced and saved **except** the ones specified.
+   *
+   * You can also provide a custom function that will be called for each key.
+   * If the function returns `true`, the key will be saved and synced.
+   *
+   * @default 'omit'
+   */
+  readonly filterKeysStrategy?: StoreKeyFilterStrategy;
+
+  /**
+   * Keys the plugin should ignore. Those won't be saved nor synced.
+   * @deprecated Use {@link TauriPluginPiniaStoreOptions.filterKeys} instead.
+   */
   readonly ignoreKeys?: string | string[];
 }
 
