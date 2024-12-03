c770d71c23252107784f6137a0e13db602ad7722
Andrew Ferreira <andrew.shien2@gmail.com>
2024-12-03 14:35:52 -0300

    refactor: move types to shared package

--- packages/shared/vite.config.js
+++ packages/shared/vite.config.js
@@ -1,20 +1,17 @@
+import { join } from 'node:path';
 import dts from 'vite-plugin-dts';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ rollupTypes: false })],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     minify: false,
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: join(__dirname, 'src/index.ts'),
       formats: ['es'],
       fileName: 'index',
-    },
-    rollupOptions: {
-      external: [/^@tauri-apps/, /^@tauri-store/, 'pinia', 'vue'],
     },
   },
 });
--- packages/tauri-plugin-pinia/src/types.ts
+++ packages/tauri-plugin-pinia/src/types.ts
@@ -1,68 +1,11 @@
 import type { WatchOptions } from 'vue';
-import type { MaybePromise } from '@tb-dev/utils';
+import type { PluginOptions, StoreOptions } from '@tauri-store/shared';
 
-export interface TauriPluginPiniaOptions {
+export interface TauriPluginPiniaOptions extends PluginOptions {
   /** @default true */
   readonly deep?: WatchOptions['deep'];
   /** @default 'pre' */
   readonly flush?: WatchOptions['flush'];
-
-  /**
-   * Custom error handler.
-   * @default console.error
-   */
-  readonly onError?: (error: unknown) => MaybePromise<void>;
-
-  /**
-   * Interval in milliseconds to use when syncing the store with the backend.
-   * This option is only valid when {@link TauriPluginPiniaOptions.syncStrategy} is set to `debounce` or `throttle`.
-   *
-   * @default 0
-   */
-  readonly syncInterval?: number;
-
-  /**
-   * Strategy to use when syncing the store with the backend.
-   *
-   * If the value is a number, the plugin will use `debounce` with the specified value as the interval.
-   * If `null`, the strategy will be `immediate`.
-   *
-   * For a detailed explanation about the differences between `debounce` and `throttle`, see:
-   * https://kettanaito.com/blog/debounce-vs-throttle
-   *
-   * @default 'immediate'
-   */
-  readonly syncStrategy?: 'debounce' | 'throttle' | 'immediate' | number | null;
 }
 
-export type StoreKeyFilter = string | string[] | RegExp;
-export type StoreKeyFilterStrategy = 'pick' | 'omit' | ((key: string) => boolean);
-
-export interface TauriPluginPiniaStoreOptions extends TauriPluginPiniaOptions {
-  /**
-   * Keys the plugin should save or ignore.
-   *
-   * The behavior depends on the value of {@link TauriPluginPiniaStoreOptions.filterKeysStrategy}.
-   */
-  readonly filterKeys?: StoreKeyFilter;
-
-  /**
-   * Strategy to use when filtering keys.
-   * - `pick`: Only the specified keys will be synced and saved.
-   * - `omit`: All keys will be synced and saved **except** the ones specified.
-   *
-   * You can also provide a custom function that will be called for each key.
-   * If the function returns `true`, the key will be saved and synced.
-   *
-   * @default 'omit'
-   */
-  readonly filterKeysStrategy?: StoreKeyFilterStrategy;
-}
-
-export type State = Record<string, unknown>;
-
-/** @internal */
-export interface ChangePayload {
-  id: string;
-  state: State;
-}
+export type TauriPluginPiniaStoreOptions = StoreOptions & TauriPluginPiniaOptions;
