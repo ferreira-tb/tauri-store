0e2e11aaab04b0227cbb716e62a10d7a4a1c0d13
Andrew Ferreira <andrew.shien2@gmail.com>
2024-12-03 15:01:35 -0300

    chore: add comment

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 import { debounce as lodashDebounce, throttle as lodashThrottle } from 'lodash-es';
 
+// FIXME: We should not depend on lodash for this. A custom implementation would be better.
 export function debounce<T extends (...args: any) => any>(f: T, wait?: number): T {
   return lodashDebounce(f, wait) as unknown as T;
 }
