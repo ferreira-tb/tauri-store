2ecab9941e6ec2d29edbc3d81c68f7ee2bb7ed14
Andrew Ferreira <andrew.shien2@gmail.com>
2025-01-05 22:45:13 -0300

    docs: add `watch` example

--- docs/src/lib/content/guide/accessing-from-rust/snippets.ts
+++ docs/src/lib/content/guide/accessing-from-rust/snippets.ts
@@ -6,7 +6,7 @@ export const get = snippet((metadata) => {
 use ${snakeCase(metadata.name)}::ManagerExt;
 
 #[tauri::command]
-async fn get_counter(app: AppHandle) -> i32 {
+fn get_counter(app: AppHandle) -> i32 {
   let value = app
     .${snakeCase(metadata.title!)}()
     .get("store", "counter")
@@ -19,12 +19,38 @@ async fn get_counter(app: AppHandle) -> i32 {
 
 export const tryGet = snippet((metadata) => {
   return `
+use ${snakeCase(metadata.name)}::ManagerExt;
+
 #[tauri::command]
-async fn try_get_counter(app: AppHandle) -> i32 {
+fn try_get_counter(app: AppHandle) -> i32 {
   app
     .${snakeCase(metadata.title!)}()
     .try_get::<i32>("store", "counter")
     .unwrap()
+}
+  `;
+});
+
+export const watchStore = snippet((metadata) => {
+  const title = snakeCase(metadata.title!);
+  return `
+use ${snakeCase(metadata.name)}::ManagerExt;
+
+#[tauri::command]
+fn watch_store(app: AppHandle) {
+  let id = app.${title}().watch("store", |handle| {
+    let counter = handle
+      .${title}()
+      .try_get::<i32>("store", "counter")?;
+
+    do_something(counter);
+    Ok(())
+  });
+
+  // The method returns an id that can be used to remove the watcher.
+  if let Ok(id) = id {
+    app.${title}().unwatch("store", id).unwrap();
+  }
 }
   `;
 });
