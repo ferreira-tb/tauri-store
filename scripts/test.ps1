b5e04248dde60605c0dbcf21f266b4310681e803
Andrew Ferreira <andrew.shien2@gmail.com>
2025-02-09 11:41:29 -0300

    ci: remove type check

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -10,10 +10,5 @@ if ($Ubuntu) {
   sudo apt-get -y install libgtk-3-dev libwebkit2gtk-4.1-dev
 }
 
-pnpm install
-pnpm run build:shared
-pnpm run svelte:sync
-pnpm run type-check
-
-cargo clippy --workspace
+cargo +nightly clippy --workspace
 cargo test -p tauri-store --tests -- --test-threads=1
