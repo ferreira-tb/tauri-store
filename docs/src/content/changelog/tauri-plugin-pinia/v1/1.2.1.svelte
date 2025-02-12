3d460ac0e2b9d51694287019825829aca7d72c10
Andrew Ferreira <andrew.shien2@gmail.com>
2025-02-12 12:46:21 -0300

    docs: add changelog for the other plugins

---
+++ docs/src/content/changelog/tauri-plugin-pinia/v1/1.2.1.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Ext, Link } from '$components/link';
+  import { Changelog } from '$components/container';
+
+  const version = '1.2.1';
+
+  const url = 'https://docs.rs/tauri-store/0.8.0/tauri_store/';
+</script>
+
+<Changelog.Version {version} />
+
+<Changelog.Dependencies {version}>
+  <ul>
+    <li>
+      Update <Ext href={url} code>tauri-store</Ext> to
+      <Link href="/tauri-store/changelog/tauri-store#v0-8-0" code>0.8.0</Link>.
+    </li>
+  </ul>
+</Changelog.Dependencies>
